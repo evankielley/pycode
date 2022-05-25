aefe57ec06166843bff8f481e62709059e027cae
evankielley <evan.kielley@woodplc.com>
2022-05-25 18:29:45 -0230

    added solution to top k frequent elements

---
+++ leetcode/top_k_frequent_elements.py
@@ -0,0 +1,104 @@
+"""Get the k most frequent elements in a list.
+
+https://leetcode.com/problems/top-k-frequent-elements/
+"""
+
+import heapq
+
+def f(nums, k, solver='bucket_sort'):
+    """Get the k most frequent elements in a list.
+
+    Parameters
+    ----------
+    nums : list of int or float
+        A list of numbers.
+    k : int
+        The number of most frequent elements to return.
+    solver : str, optional
+        The solver to use to obtain the solution, by default 'bucket_sort'.
+
+    Returns
+    -------
+    list of int or float
+        The k most frequent elements in the list.
+
+    Raises
+    ------
+    NotImplementedError
+        Raises this error if the solver specified is not recognized.
+
+    Example
+    -------
+    >>> f([1,1,1,2,2,3], 2)
+    [1,2]
+    """
+    solver_map = {
+        'bucket_sort': _bucket_sort_solver,
+        'heap': _heap_solver,
+    }
+    if not solver in solver_map:
+        raise NotImplementedError(f'Unrecognized solver: {solver}. Must be one of {list(solver_map.keys())}')
+    return solver_map[solver](nums, k)
+
+def _bucket_sort_solver(nums, k):
+    """Get the k most frequent elements in a list by using a reverse bucket sort algorithm.
+
+    Time: O(n)
+    Space: O(n)
+
+    Parameters
+    ----------
+    nums : list of int or float
+        A list of numbers.
+    k : int
+        The number of most frequent elements to return.
+
+    Returns
+    -------
+    list of int or float
+        The k most frequent elements in the list.
+    """
+    counter = {}
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+    arr = [[] for _ in range(len(nums))]
+    for num, count in counter.items():
+        arr[count-1].append(num)
+    output = []
+    for _nums in arr[::-1]:
+        if len(output) == k:
+            break
+        if len(_nums) > 0:
+            output += _nums
+    return output
+
+def _heap_solver(nums, k):
+    """Get the k most frequent elements in a list by using a max heap.
+
+    Time: O(nlogn)
+    Space: O(n)
+
+    Parameters
+    ----------
+    nums : list of int or float
+        A list of numbers.
+    k : int
+        The number of most frequent elements to return.
+
+    Returns
+    -------
+    list of int or float
+        The k most frequent elements in the list.
+    """
+    counter = dict()
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+    hashmap = dict()
+    for num, count in counter.items():
+        hashmap[count] = hashmap.get(count, []) + [num]
+    max_heap = [(-count, _nums) for count, _nums in hashmap.items()]
+    heapq.heapify(max_heap)
+    output = []
+    while len(output) < k:
+        output += heapq.heappop(max_heap)[1]
+    return output
---
+++ tests/test_leetcode_top_k_frequent_elements.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.top_k_frequent_elements import f
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.f = f
+    def test_1(self):
+        self.assertListEqual(f([1,1,1,2,2,3], 2), [1,2])
+    def test_2(self):
+        self.assertListEqual(f([1], 1), [1])
+    def test_3(self):
+        self.assertListEqual(f([1.1,1.1,1.1,2,2,3], 2), [1.1,2])
+
+
+if __name__ == "__main__":
+    unittest.main()
